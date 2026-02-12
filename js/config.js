a45cc4a25f12878c20363a1cb270e11d1a285030
taropafadzw <46368995+taropafadzw@users.noreply.github.com>
2026-02-12 03:22:54 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_rHQRc91zQ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6mlao6jb0vb1va3klhirv8a956', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
